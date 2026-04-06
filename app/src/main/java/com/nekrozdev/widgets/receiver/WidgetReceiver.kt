483867d497b2a824d732f47b2aa2e87f3e9fa967
evgeniynekroz <evgen20111110@gmail.com>
2026-04-07 01:31:23 +0300

    Create WidgetReceiver.kt

---
+++ app/src/main/java/com/nekrozdev/widgets/receiver/WidgetReceiver.kt
@@ -0,0 +1,8 @@
+package com.nekrozdev.widgets.receiver
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class WidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = UniversalWidget()
+}
