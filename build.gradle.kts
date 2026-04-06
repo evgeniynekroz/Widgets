cf07ebd4b5a6aabc02f3c78cdfc267f32c228582
evgeniynekroz <evgen20111110@gmail.com>
2026-04-07 02:20:44 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    // Версия 8.2.0 хорошо работает с Gradle 8.5
     id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
