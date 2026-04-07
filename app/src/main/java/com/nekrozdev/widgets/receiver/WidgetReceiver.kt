803c4bc6b551d5f074d593157802c0ad26699da7
evgeniynekroz <evgen20111110@gmail.com>
2026-04-07 16:25:17 +0300

    Update WidgetReceiver.kt

--- app/src/main/java/com/nekrozdev/widgets/receiver/WidgetReceiver.kt
+++ app/src/main/java/com/nekrozdev/widgets/receiver/WidgetReceiver.kt
@@ -1,12 +1,15 @@
 package com.nekrozdev.widgets.receiver
 
+import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
+// ВАЖНО: Тот же импорт здесь
+import androidx.glance.appwidget.updateAll 
 
 class WidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget = WidgetUI()
     
-    // Добавляем метод для обновления из воркера
-    suspend fun updateAll(context: android.content.Context) {
+    suspend fun updateAll(context: Context) {
+        // Теперь это скомпилируется
         glanceAppWidget.updateAll(context)
     }
 }
